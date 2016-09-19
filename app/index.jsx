502535ffbca96ec1f31c08dc66bed70649e4b22c
Chris Cooper <chris@propelleraero.com.au>
2016-09-19 14:28:37 +1000

    redux todos example

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>
-  }
-}
+const store = createStore(reducer)
 
-render(<App/>, document.getElementById('app'))
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)

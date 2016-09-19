de260053eb3034ee39f8d07cc0a0eb4814738cb3
Chris Cooper <chris@propelleraero.com.au>
2016-09-19 14:38:57 +1000

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,22 @@ var BUILD_DIR = path.resolve(__dirname, './public');
 var APP_DIR = path.resolve(__dirname, './app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel'
-      }
-    ]
-  }
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            include: APP_DIR,
+            loader: 'babel',
+            include: __dirname
+        }]
+    }
 };
 
 module.exports = config;

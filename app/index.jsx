d62b7aba3afa1e38ac153da814eb86e58b0393a6
Chris Cooper <chris@propelleraero.com.au>
2016-09-19 14:44:28 +1000

    hook in redux logger

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
+import createLogger from 'redux-logger'
 
-const store = createStore(reducer)
+const logger = createLogger()
+const store = createStore(reducer, applyMiddleware(logger))
 
 render(
   <Provider store={store}>
